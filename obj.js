d50f16ba69ce357caf932731cd1a3d420b0d6c46
Stuart Urback <stuart@urback.net>
2016-05-10 11:24:07 -0600

    Practicing defining properties

--- obj.js
+++ obj.js
@@ -1,21 +1,53 @@
-"use strict"
-
-// function Test(thing){
-//   this.test = 'test'
-//   this.things = []
-//   this.things.push(thing)
+// "use strict"
+//
+// // function Test(thing){
+// //   this.test = 'test'
+// //   this.things = []
+// //   this.things.push(thing)
+// // }
+// var Test = {}
+//
+// Test.prototype.addThings = function(thingToAdd){
+//   this.things.push(thingToAdd)
+//   console.log(thingToAdd)
 // }
-var Test = {}
-
-Test.prototype.addThings = function(thingToAdd){
-  this.things.push(thingToAdd)
-  console.log(thingToAdd)
-}
-
-var container = Object.create(Test.prototype, {
-
-})
-
-console.log(container)
+//
+// var container = Object.create(Test.prototype, {
+//
+// })
+//
+// console.log(container)
 
 // container.addThings('bing')
+
+// using defineProperty method
+
+
+var newContainer = {}
+
+Object.defineProperty(newContainer, "test", {
+  value: 35,
+  writable: true,
+  enumerable: false,
+  configurable: true
+})
+
+Object.defineProperty(newContainer, "zest", {
+  value: 36,
+  writable: true,
+  enumerable: true,
+  configurable: true
+})
+
+Object.defineProperty(newContainer, "best", {
+  value: 37,
+  writable: true,
+  enumerable: true,
+  configurable: false
+})
+
+delete newContainer.best
+
+// newContainer["test"] = 35
+
+console.log(newContainer)
